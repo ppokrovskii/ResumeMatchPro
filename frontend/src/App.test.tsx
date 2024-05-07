e05be52820568e257cd8a9b995703f86b09aaffe
ppokrovskii <38814050+ppokrovskii@users.noreply.github.com>
2024-05-07 22:31:24 +0400

    Refactor FilesPage component to use 'JD' instead of 'CV' for file retrieval

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,6 +4,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  // find div with class 'app'
+  const appDiv = screen.getByRole('main');
+  expect(appDiv).toBeInTheDocument();
+}
+);
+
