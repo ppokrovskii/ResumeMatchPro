c29ae747141617695623c3033b2f009e953b62cd
ppokrovskii <38814050+ppokrovskii@users.noreply.github.com>
2024-06-02 18:03:52 +0400

    chore: Remove unused project images

--- landing/src/components/Sections/Projects.jsx
+++ landing/src/components/Sections/Projects.jsx
@@ -7,10 +7,10 @@ import FullButton from "../Buttons/FullButton";
 import ProjectImg1 from "../../assets/img/projects/1.png";
 import ProjectImg2 from "../../assets/img/projects/2.png";
 import ProjectImg3 from "../../assets/img/projects/3.png";
-import ProjectImg4 from "../../assets/img/projects/4.png";
-import ProjectImg5 from "../../assets/img/projects/5.png";
-import ProjectImg6 from "../../assets/img/projects/6.png";
-import AddImage2 from "../../assets/img/add/add2.png";
+// import ProjectImg4 from "../../assets/img/projects/4.png";
+// import ProjectImg5 from "../../assets/img/projects/5.png";
+// import ProjectImg6 from "../../assets/img/projects/6.png";
+// import AddImage2 from "../../assets/img/add/add2.png";
 
 export default function Projects({handleOpenContactForm}) {
   return (
