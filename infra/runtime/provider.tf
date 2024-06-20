fe7c66c9cb134c79fb70e400ec653aa3441df833
ppokrovskii <38814050+ppokrovskii@users.noreply.github.com>
2024-06-20 23:03:17 +0400

    chore: Update publish-profile for landing-backend.yml and terraform.lock.hcl

--- infra/runtime/provider.tf
+++ infra/runtime/provider.tf
@@ -1,3 +1,19 @@
 provider "azurerm" {
     features {}
 }
+
+provider "github" {
+    token = var.github_token
+    owner = var.github_owner
+    version = "~> 5.0"
+}
+
+variable "github_token" {
+  description = "GitHub token with appropriate permissions"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_owner" {
+    description = "The GitHub organization or user name"
+}
