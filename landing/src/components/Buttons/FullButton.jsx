8e62e98f5ea54a008d88fadb057ed0d626df0b5c
ppokrovskii <38814050+ppokrovskii@users.noreply.github.com>
2024-06-23 21:57:12 +0400

    chore: Refactor FullButton component styles for responsiveness

--- landing/src/components/Buttons/FullButton.jsx
+++ landing/src/components/Buttons/FullButton.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function FullButton({ title, action, border, borderWidth = "1px", color="#fff", backgroundColor = "#580cd2" }) {
+export default function FullButton({ title, action, border, borderWidth = "1px", color = "#fff", backgroundColor = "#580cd2" }) {
   return (
     <Wrapper
-      className="animate pointer radius8"
+      className="animate pointer"
       onClick={action ? () => action() : null}
       $border={border}
       $borderWidth={borderWidth}
       $color={color}
-      style={{ backgroundColor: backgroundColor }}
+      $backgroundColor={backgroundColor}
     >
       {title}
     </Wrapper>
@@ -19,14 +19,28 @@ export default function FullButton({ title, action, border, borderWidth = "1px",
 const Wrapper = styled.button`
   border: ${(props) => (props.$border ? props.$border : "none")};
   background-color: ${(props) => (props.$backgroundColor ? props.$backgroundColor : "transparent")};
-  max-width: 220px;
-  width: 100%;  
-  padding: 15px;
+  border-radius: 12px;
+  max-width: 50vw;
+  width: 100%;
+  padding: 1vw 2vw;
   outline: none;
   color: ${(props) => (props.$color ? props.$color : "#fff")};
+  font-size: 16px;
+  
   :hover {
     background-color: ${(props) => (props.$border ? "transparent" : "#580cd2")};
     border: 1px solid #7620ff;
     color: ${(props) => (props.$color ? props.$color : "#fff")};
+  }
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+    padding: 12px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 12px;
+    padding: 10px;
+    max-width: 180px;
   }
 `;
