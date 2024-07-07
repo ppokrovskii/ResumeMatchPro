d02b80c72902645e0ecc34c3675c4e98ee081e90
ppokrovskii <38814050+ppokrovskii@users.noreply.github.com>
2024-07-07 06:56:48 +0400

    Refactor Nav component to use Header component for improved code organization and consistency

--- landing_frontend/src/app/page.tsx
+++ landing_frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 
-import Nav from "./components/Nav";
+import Header from "./components/layout/Header/Header";
 import Main from "./components/layout/Main/Main";
 import Footer from "./components/layout/Footer/Footer";
 
@@ -9,7 +9,7 @@ import Footer from "./components/layout/Footer/Footer";
 export default function Home() {
   return (
     <div className="w-full bg-bg flex flex-col items-center justify-between min-h-screen ">
-      <Nav/>
+      <Header/>
       <Main />
       <Footer />
     </div>
