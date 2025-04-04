9aba89d13500f8f45ffd33ffdb2a816e5465bf40
ppokrovskii <38814050+ppokrovskii@users.noreply.github.com>
2025-04-04 16:37:05 +0400

    Refactor AsyncTelegramWebhookHandler in logger.py to revert to a simpler message format by utilizing the standard logging representation. This change enhances maintainability and aligns with previous efforts to streamline the logging process while ensuring clarity in error notifications.

--- azfunctions/shared/logger.py
+++ azfunctions/shared/logger.py
@@ -1,9 +1,6 @@
 import asyncio
-import json
 import logging
 import os
-from datetime import datetime
-from email import message
 from typing import Optional
 
 import aiohttp
@@ -28,37 +25,9 @@ class AsyncTelegramWebhookHandler(logging.Handler):
     async def _emit_async(self, record: logging.LogRecord):
         try:
             session = await self._get_session()
-            # message = {
-            #     "schemaId": "azureMonitorCommonAlertSchema",
-            #     "data": {
-            #         "essentials": {
-            #             "alertId": f"log-{record.created}",
-            #             "alertRule": f"Custom Logger Alert - {record.levelname}",
-            #             "severity": "Error"
-            #             if record.levelno >= logging.ERROR
-            #             else "Warning",
-            #             "signalType": "Log",
-            #             "monitorCondition": "Fired",
-            #             "monitorService": "Custom Logger",
-            #             "targetResource": os.getenv("WEBSITE_SITE_NAME", "Unknown"),
-            #             "targetResourceType": "Microsoft.Web/sites",
-            #             "targetResourceGroup": os.getenv(
-            #                 "WEBSITE_RESOURCE_GROUP", "Unknown"
-            #             ),
-            #             "targetResourceName": os.getenv("WEBSITE_SITE_NAME", "Unknown"),
-            #             "timestamp": datetime.fromtimestamp(record.created).isoformat(),
-            #         },
-            #         "alertContext": {
-            #             "LogLevel": record.levelname,
-            #             "Logger": record.name,
-            #             "Message": record.getMessage(),
-            #             "Function": record.funcName,
-            #             "LineNumber": record.lineno,
-            #             "FilePath": record.pathname,
-            #         },
-            #     },
-            # }
-            message = str(record)
+            # Format the record using the default formatter
+            formatted_message = self.format(record)
+            message = {"message": formatted_message}
 
             async with session.post(self.webhook_url, json=message) as response:
                 if response.status not in (200, 201, 202):
